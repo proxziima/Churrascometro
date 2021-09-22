b65e4724ddf111fc766e0bcb85083a1608ed475c
QueirozVinicius <felipevs.queiroz@gmail.com>
2021-09-22 18:00:44 -0300

    :sparkles: Finished the Accordion animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,37 @@
 /*===== BASE DE CÁLCULO =====*/
 
 /*===== ACCORDION SHOW AND HIDE =====*/
+// Seleciona todos os Accordions Itens
 const accordionItems = document.querySelectorAll('.accordion__item');
 
-accordionItems.forEach((item)) => {
-    
+// Selectiona cada Item
+accordionItems.forEach((item) => {
+    //Selection o header do Item
+    const accordionHeader = item.querySelector('.accordion__header');
+    //Seta o evento do click
+    accordionHeader.addEventListener('click', () =>{
+        
+        const openItem = document.querySelector('.accordion-open')
+
+        //chama a função Toggle
+        toggleItem(item)
+
+        //valida se a classe existe
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.accordion__content');
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style');
+        item.classList.remove('accordion-open');
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+
 }
